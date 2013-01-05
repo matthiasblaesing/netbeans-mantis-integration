9cd260b9cd0001e390892f8f1d5cd94ec4354a68
Matthias Bläsing <mblaesing@doppel-helix.eu>
2013-01-05 15:43:06 +0100

    Enable opening bugs from the relationship panel

--- src/main/java/eu/doppel_helix/netbeans/mantisintegration/swing/RelationshipDisplay.java
+++ src/main/java/eu/doppel_helix/netbeans/mantisintegration/swing/RelationshipDisplay.java
@@ -2,22 +2,27 @@
 package eu.doppel_helix.netbeans.mantisintegration.swing;
 
 import biz.futureware.mantisconnect.RelationshipData;
+import eu.doppel_helix.netbeans.mantisintegration.Mantis;
+import eu.doppel_helix.netbeans.mantisintegration.MantisConnector;
 import eu.doppel_helix.netbeans.mantisintegration.issue.MantisIssue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import org.netbeans.modules.bugtracking.api.Repository;
 import org.netbeans.modules.bugtracking.util.LinkButton;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 
 public class RelationshipDisplay extends DelegatingBaseLineJPanel implements ActionListener {
     private final String COMMAND_DELETE = "deleteString";
+    private final String COMMAND_OPENISSUE = "openIssue";
     private RelationshipData rd;
     private MantisIssue issue;
-    private JLabel leadingLabel = new JLabel();
+    private LinkButton bugButton = new LinkButton();
     private LinkButton deleteButton = new LinkButton("delete");
     private JLabel trailingLabel = new JLabel();
+    private JLabel middleLabel = new JLabel();
 
     public RelationshipDisplay(MantisIssue issue, RelationshipData rd) {
         super();
@@ -25,12 +30,16 @@ public class RelationshipDisplay extends DelegatingBaseLineJPanel implements Act
         setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
         this.rd = rd;
         this.issue = issue;
-        leadingLabel.setText(String.format(
-                "%s %d (",
+        bugButton.setText(String.format(
+                "%s %d",
                 rd.getType().getName(),
                 rd.getTarget_id()));
+        bugButton.addActionListener(this);
+        bugButton.setActionCommand(COMMAND_OPENISSUE);
+        middleLabel.setText(" (");
         trailingLabel.setText(")");
-        this.add(leadingLabel);
+        this.add(bugButton);
+        this.add(middleLabel);
         this.add(deleteButton);
         deleteButton.addActionListener(this);
         deleteButton.setActionCommand(COMMAND_DELETE);
@@ -48,6 +57,25 @@ public class RelationshipDisplay extends DelegatingBaseLineJPanel implements Act
                     } catch (Exception ex) {
                         NotifyDescriptor nd = new NotifyDescriptor.Exception(ex,
                                 "Failed to remove relationship from issue");
+                        DialogDisplayer.getDefault().notifyLater(nd);
+                    }
+                }
+            });
+        } else if (COMMAND_OPENISSUE.equals(e.getActionCommand())) {
+            issue.getMantisRepository().getRequestProcessor().submit(new Runnable() {
+                public void run() {
+                    try {
+                        Repository r = Mantis.getInstance().getBugtrackingFactory().getRepository(
+                                MantisConnector.ID,
+                                issue.getMantisRepository().getInfo().getId());
+
+                        MantisIssue mi = issue.getMantisRepository().
+                                getIssues(rd.getTarget_id())[0];
+
+                        Mantis.getInstance().getBugtrackingFactory().openIssue(r, mi);
+                    } catch (Exception ex) {
+                        NotifyDescriptor nd = new NotifyDescriptor.Exception(ex,
+                                "Failed to open issue");
                         DialogDisplayer.getDefault().notifyLater(nd);
                     }
                 }
