matthiasblaesing/netbeans-mantis-integration

commit 2a0538f478895e7f8fef46f596d746938813d9f8

Delegate non special cases in CellRenderer to default renderer (QueryTableCellRenderer), fixes display of read-attribute

src/main/java/eu/doppel_helix/netbeans/mantisintegration/query/MantisQueryTableCellRenderer.java
@@ -15,11 +15,17 @@ import java.util.Map;
 import javax.swing.JComponent;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableCellRenderer;
 import org.netbeans.modules.bugtracking.issuetable.IssueNode;
 
 public class MantisQueryTableCellRenderer extends DefaultTableCellRenderer {
+    private TableCellRenderer superRenderer;
     private Map<BigInteger,Color> colorMap = Mantis.getInstance().getStatusColorMap();
     DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+
+    public MantisQueryTableCellRenderer(TableCellRenderer superRenderer) {
+        this.superRenderer = superRenderer;
+    }
     
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
@@ -27,8 +33,8 @@ public class MantisQueryTableCellRenderer extends DefaultTableCellRenderer {
         Color overrideColor = Color.WHITE;
         if(value instanceof IssueNode.IssueProperty) {
             try {
-                value = ((IssueNode.IssueProperty) value).getValue();
                 if ("mantis.issue.status".equals(((IssueNode.IssueProperty) originalValue).getName())) {
+                    value = ((IssueNode.IssueProperty) value).getValue();
                     if (value instanceof ObjectRef) {
                         ObjectRef or = (ObjectRef) value;
                         BigInteger level = or.getId();
@@ -53,7 +59,7 @@ public class MantisQueryTableCellRenderer extends DefaultTableCellRenderer {
             value = df.format(((Calendar) value).getTime());
         }
         
-        Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        Component c = superRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
         
         if(overrideColor != null) {
         if(c instanceof JComponent) {

src/main/java/eu/doppel_helix/netbeans/mantisintegration/query/MantisQueryController.java
@@ -235,7 +235,7 @@ public class MantisQueryController extends QueryController implements ActionList
             }
                 
             };
-        issueTable.setRenderer(new MantisQueryTableCellRenderer());
+        issueTable.setRenderer(new MantisQueryTableCellRenderer(issueTable.getRenderer()));
         // IssueTables relies on initialized Columns, but dispatches initColumns
         // via an invokeLater ... -- not sure whether this is intended or a bug
         // @todo: investigate!

commit 1bea796ea28de460740e5bd640d542bdc238e6c5

Fix issues in IssueTable rendering introduced with commit 2a0538f478

src/main/java/eu/doppel_helix/netbeans/mantisintegration/query/MantisQueryTableCellRenderer.java
@@ -33,13 +33,15 @@ public class MantisQueryTableCellRenderer extends DefaultTableCellRenderer {
         Color overrideColor = Color.WHITE;
         if(value instanceof IssueNode.IssueProperty) {
             try {
-                if ("mantis.issue.status".equals(((IssueNode.IssueProperty) originalValue).getName())) {
-                    value = ((IssueNode.IssueProperty) value).getValue();
-                    if (value instanceof ObjectRef) {
-                        ObjectRef or = (ObjectRef) value;
-                        BigInteger level = or.getId();
-                        if(colorMap.get(level) != null) {
-                            overrideColor = colorMap.get(level);
+		if (((IssueNode.IssueProperty) originalValue).getName().startsWith("mantis.issue.")) {
+		    value = ((IssueNode.IssueProperty) value).getValue();
+                    if ("mantis.issue.status".equals(((IssueNode.IssueProperty) originalValue).getName())) {
+                        if (value instanceof ObjectRef) {
+                            ObjectRef or = (ObjectRef) value;
+                            BigInteger level = or.getId();
+                            if(colorMap.get(level) != null) {
+                                overrideColor = colorMap.get(level);
+                            }
                         }
                     }
                 }
@@ -59,7 +61,12 @@ public class MantisQueryTableCellRenderer extends DefaultTableCellRenderer {
             value = df.format(((Calendar) value).getTime());
         }
         
-        Component c = superRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        Component c = null;
+	if(value instanceof IssueNode.IssueProperty) {
+		c = superRenderer.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+	} else {
+		c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+	}
         
         if(overrideColor != null) {
         if(c instanceof JComponent) {