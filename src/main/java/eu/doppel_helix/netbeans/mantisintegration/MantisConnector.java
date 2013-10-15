2cb672bc78723d3757d2974b5131db2f63f7ff95
Matthias Bläsing <mblaesing@doppel-helix.eu>
2013-10-15 22:51:01 +0200

    Remove workaround introduced for 7.2

--- src/main/java/eu/doppel_helix/netbeans/mantisintegration/MantisConnector.java
+++ src/main/java/eu/doppel_helix/netbeans/mantisintegration/MantisConnector.java
@@ -4,7 +4,6 @@ package eu.doppel_helix.netbeans.mantisintegration;
 import eu.doppel_helix.netbeans.mantisintegration.repository.MantisRepository;
 import org.netbeans.modules.bugtracking.api.Repository;
 import org.netbeans.modules.bugtracking.spi.BugtrackingConnector;
-import org.netbeans.modules.bugtracking.spi.IssueFinder;
 import org.netbeans.modules.bugtracking.spi.RepositoryInfo;
 import org.openide.util.NbBundle;
 
@@ -41,24 +40,5 @@ public class MantisConnector extends BugtrackingConnector {
                 m.getRepositoryProvider(),
                m.getQueryProvider(),
                 m.getIssueProvider());
-    }
-
-    // Dirty workaround for bug fixed in 7.3 cycle - make it work in 7.2...
-    // @todo: When 7.4 is released or we implement a real Finder get rid of this hack
-    private final IssueFinder noopIssueFinder = new IssueFinder() {
-        @Override
-        public int[] getIssueSpans(CharSequence cs) {
-            return new int[0];
-        }
-
-        @Override
-        public String getIssueId(String string) {
-            return "";
-        }
-    };
-    
-    @Override
-    public IssueFinder getIssueFinder() {
-        return noopIssueFinder;
     }
 }
